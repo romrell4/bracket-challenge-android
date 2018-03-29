290590af0041fe35454db954868c8874ee4713f7
Eric Romrell <eric_romrell@byu.edu>
2018-03-28 20:24:58 -0600

    Loaded up all rounds

--- app/src/main/java/com/romrell4/bracketchallenge/controller/MyBracketFragment.kt
+++ app/src/main/java/com/romrell4/bracketchallenge/controller/MyBracketFragment.kt
@@ -2,6 +2,7 @@ package com.romrell4.bracketchallenge.controller
 
 import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.view.PagerAdapter
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -29,6 +30,8 @@ class MyBracketFragment: Fragment() {
         private const val TOURNAMENT_EXTRA = "tournament"
         private const val VIEW_BRACKET_INDEX = 0
         private const val CREATE_BRACKET_INDEX = 1
+        private const val SPINNER_INDEX = 0
+        private const val ROUNDS_INDEX = 1
 
         fun newInstance(tournament: Tournament): MyBracketFragment {
             val fragment = MyBracketFragment()
@@ -40,16 +43,14 @@ class MyBracketFragment: Fragment() {
     }
 
     private lateinit var myView: View
-    private val adapter = MatchAdapter()
     private lateinit var tournament: Tournament
+    private var bracket: Bracket? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_my_bracket, container, false)
 
         view?.run {
             myView = this
-            recyclerView.layoutManager = LinearLayoutManager(activity)
-            recyclerView.adapter = adapter
 
             tournament = arguments.getParcelable(TOURNAMENT_EXTRA)
             loadBracket()
@@ -59,14 +60,12 @@ class MyBracketFragment: Fragment() {
     }
 
     private fun loadBracket() {
-        myView.swipeRefreshLayout.isRefreshing = true
+        myView.matchesViewSwitcher.displayedChild = SPINNER_INDEX
         Client.createApi().getMyBracket(tournament.tournamentId).enqueue(object: Callback<Bracket> {
             override fun onResponse(call: Call<Bracket>?, response: Response<Bracket>?) {
-                myView.swipeRefreshLayout.isRefreshing = false
-                myView.swipeRefreshLayout.isEnabled = false
-
                 //If we have a bracket, view it. Otherwise, view the create bracket UI
-                response?.body()?.rounds?.get(0)?.let {
+                bracket = response?.body()
+                bracket?.rounds?.let {
                     setupViewBracketUI(it)
                 } ?: response?.code()?.let {
                     if (it == 404) {
@@ -81,10 +80,11 @@ class MyBracketFragment: Fragment() {
         })
     }
 
-    private fun setupViewBracketUI(matches: List<Match>) {
+    private fun setupViewBracketUI(rounds: List<List<Match>>) {
         myView.viewSwitcher.displayedChild = VIEW_BRACKET_INDEX
-        adapter.matches = matches
-        adapter.notifyDataSetChanged()
+        myView.matchesViewSwitcher.displayedChild = ROUNDS_INDEX
+
+        myView.viewPager.adapter = RoundPagerAdapter(rounds)
     }
 
     private fun setupCreateBracketUI() {
@@ -102,7 +102,23 @@ class MyBracketFragment: Fragment() {
         }
     }
 
-    inner class MatchAdapter(var matches: List<Match> = emptyList()): RecyclerView.Adapter<MatchAdapter.ViewHolder>() {
+    inner class RoundPagerAdapter(private val rounds: List<List<Match>>): PagerAdapter() {
+        override fun getCount() = rounds.size
+        override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) = container.removeView(`object` as? View)
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val roundView = activity.layoutInflater.inflate(R.layout.view_round, container, false)
+            (roundView as? RecyclerView?)?.let {
+                it.layoutManager = LinearLayoutManager(activity)
+                it.adapter = MatchAdapter(position, rounds[position])
+            }
+            container.addView(roundView)
+            return roundView
+        }
+    }
+
+    inner class MatchAdapter(private val round: Int, private val matches: List<Match> = emptyList()): RecyclerView.Adapter<MatchAdapter.ViewHolder>() {
         override fun getItemCount() = matches.size
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(activity.layoutInflater.inflate(R.layout.row_match, parent, false))
         override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(matches[position])
@@ -115,9 +131,9 @@ class MyBracketFragment: Fragment() {
 
             fun bind(match: Match) {
                 nameTextView1.text = match.player1Full()
-                checkmark1.visibility = if (match.player1Id == match.winnerId) View.VISIBLE else View.GONE
+                checkmark1.visibility = if (match.player1Id != null && match.player1Id == match.winnerId) View.VISIBLE else View.GONE
                 nameTextView2.text = match.player2Full()
-                checkmark2.visibility = if (match.player2Id == match.winnerId) View.VISIBLE else View.GONE
+                checkmark2.visibility = if (match.player2Id != null && match.player2Id == match.winnerId) View.VISIBLE else View.GONE
             }
         }
     }
